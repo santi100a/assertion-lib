2636824c64cd99a72b6b9f2e9375699e2874ddb1
santi100a <95766532+santi100a@users.noreply.github.com>
2023-05-24 18:59:02 -0500

    lint: modified code to comply with ESLint infra: modified yarn.lock and ci.yml

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { deepEquality } from '@santi100/equal-lib';
 
-function indexOf(arr: any[], item: any) {
+function indexOf(arr: unknown[], item: unknown) {
 	for (const idx in arr) {
 		if (deepEquality(arr[idx], item)) return arr[idx];
 	}
@@ -101,7 +101,7 @@ export interface AssertOptionalParams<E, A> {
  * @param expectedType The expected type. Must be one of {@link Type}.
  * @param name An optional expression name to be put in the `TypeError`'s message. Defaults to "arg".
  */
-export function assertTypeOf(arg: any, expectedType: Type, name = 'arg') {
+export function assertTypeOf(arg: unknown, expectedType: Type, name = 'arg') {
 	const TYPES = [
 		'string',
 		'number',
@@ -128,8 +128,8 @@ export function assertTypeOf(arg: any, expectedType: Type, name = 'arg') {
  * @param shallow Whether or not to use shallow equality (default deep equality is powered by 
    `@santi100/equal-lib` 😉).
  */
-export function assertOneOf<T = unknown>(arg: any, name: string, choices: T[], shallow = false) {
-	if (shallow ? choices.indexOf(arg) : indexOf(choices, arg) === -1)
+export function assertOneOf<T = unknown>(arg: unknown, name: string, choices: T[], shallow = false) {
+	if (shallow ? choices.indexOf(arg as T) : indexOf(choices, arg) === -1)
 		throw new TypeError(
 			`"${name}" must be one of "${choices.join(
 				', '
@@ -158,7 +158,8 @@ export function assertInteger(arg: number, name = 'arg') {
  * @param name An expression name to be put in the `TypeError`'s message.
  * @param min The minimum value for `arg`.
  */
-export function assertMin(arg: any, name: string, min: any) {
+export function assertMin(arg: unknown, name: string, min: unknown) {
+	// @ts-expect-error: It's alright to have these be any type.
 	if (arg < min)
 		throw new RangeError(
 			`"${name}" must be bigger than or equal to ${min}. Got "${arg}" of type "${typeof arg}".`
@@ -173,7 +174,8 @@ export function assertMin(arg: any, name: string, min: any) {
 
  * @param max The maximum value for `arg`.
  */
-export function assertMax(arg: any, name: string, max: any) {
+export function assertMax(arg: unknown, name: string, max: unknown) {
+	// @ts-expect-error: It's alright to have these be any type.
 	if (arg > max)
 		throw new RangeError(
 			`"${name}" must be smaller than or equal to ${max}. Got "${arg}" of type "${typeof arg}".`
@@ -187,7 +189,8 @@ export function assertMax(arg: any, name: string, max: any) {
  * @param min The minimum value for `arg`.
  * @param max The maximum value for `arg`.
  */
-export function assertRange(arg: any, name: string, min: any, max: any) {
+export function assertRange(arg: unknown, name: string, min: unknown, max: unknown) {
+	// @ts-expect-error: It's alright to have these be any type.
 	if (arg > max || arg < min)
 		throw new RangeError(
 			`"${name}" must be smaller than ${max} and bigger than ${min}. Got "${arg}" of type "${typeof arg}".`
@@ -199,7 +202,7 @@ export function assertRange(arg: any, name: string, min: any, max: any) {
  * @param arg Any value.
  * @param name An optional expression name to be put in the `TypeError`'s message. Defaults to "arg".
  */
-export function assertArray(arg: any, name = 'arg') {
+export function assertArray(arg: unknown, name = 'arg') {
 	if (!(arg instanceof Array))
 		throw new TypeError(
 			`"${name}" must be an Array. Got "${arg}" of type "${typeof arg}".`
@@ -213,9 +216,9 @@ export function assertArray(arg: any, name = 'arg') {
  * @param name An optional expression name to be put in the `TypeError`'s message. Defaults to "arg".
  * @since 1.0.8
  */
-export function assertInstanceOf<T = any>(
-	arg: any,
-	clas: new (...args: any[]) => T,
+export function assertInstanceOf<T = unknown>(
+	arg: unknown,
+	clas: new (...args: unknown[]) => T,
 	name = 'arg'
 ) {
 	try {
@@ -240,7 +243,8 @@ export function assertInstanceOf<T = any>(
  * @param name An optional expression name to be put in the `TypeError`'s message. Defaults to "arg".
  * @since 1.0.8
  */
-export function assertExclusiveMax(arg: any, max: any, name = 'arg') {
+export function assertExclusiveMax(arg: unknown, max: unknown, name = 'arg') {
+	// @ts-expect-error: It's alright to have these be any type.
 	if (arg >= max)
 		throw new TypeError(
 			`"${name}" must be smaller than ${max}. Got ${arg} of type ${typeof arg}.`
@@ -254,7 +258,8 @@ export function assertExclusiveMax(arg: any, max: any, name = 'arg') {
  * @param name An optional expression name to be put in the `TypeError`'s message. Defaults to "arg".
  * @since 1.0.8
  */
-export function assertExclusiveMin(arg: any, min: any, name = 'arg') {
+export function assertExclusiveMin(arg: unknown, min: unknown, name = 'arg') {
+	// @ts-expect-error: It's alright to have these be any type.
 	if (arg <= min)
 		throw new TypeError(
 			`"${name}" must be bigger than ${min}. Got ${arg} of type ${typeof arg}.`
